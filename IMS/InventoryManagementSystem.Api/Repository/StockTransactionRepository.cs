244bee98a22ac41a34a5494d63c51f348f313c01
Fahad Hasan <hasan.fahad67@outlook.com>
2026-03-11 15:57:38 +0600

    Stock Tracsaction Sql writting for crud

--- IMS/InventoryManagementSystem.Api/Repository/StockTransactionRepository.cs
+++ IMS/InventoryManagementSystem.Api/Repository/StockTransactionRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using IMS.Shared.Models.DtoModel;
 using InventoryManagementSystem.Api.Interfaces.IRepository;
 using InventoryManagementSystem.Api.Models.Entities;
 using System.Data;
@@ -31,6 +32,55 @@ namespace InventoryManagementSystem.Api.Repository
             {
                 stockTransactionId
             });
+        }
+
+        public async Task<long> AddStockTransactionAsync(StockTransactionDto stockTransactionDto)
+        {
+            var sql = @"INSERT INTO StockTransactions
+               (ProductId, TransactionType, Quantity, TransactionDate, ReferenceType, ReferenceId, CreatedBy)
+               OUTPUT INSERTED.TransactionId
+               VALUES
+               (@ProductId, @TransactionType, @Quantity, @TransactionDate, @ReferenceType, @ReferenceId, @CreatedBy)";
+
+            _connection.Open();
+
+            var result = await _connection.ExecuteScalarAsync<long>(sql, stockTransactionDto);
+
+            _connection.Close();
+
+            return result;
+        }
+
+        public async Task<int> UpdateStockTransactionAsync(StockTransactionDto stockTransactionDto)
+        {
+            var sql = @"UPDATE StockTransactions SET 
+                ProductId = @ProductId,
+                TransactionType = @TransactionType,
+                Quantity = @Quantity,
+                TransactionDate = @TransactionDate,
+                ReferenceType = @ReferenceType,
+                ReferenceId = @ReferenceId,
+                ModifiedBy = @ModifiedBy,
+                ModifiedAt = GETDATE()
+                WHERE TransactionId = @TransactionId";
+
+            _connection.Open();
+
+            var result = await _connection.ExecuteAsync(sql, new
+            {
+                TransactionId = stockTransactionDto.TransactionId,
+                ProductId = stockTransactionDto.ProductId,
+                TransactionType = stockTransactionDto.TransactionType,
+                Quantity = stockTransactionDto.Quantity,
+                TransactionDate = stockTransactionDto.TransactionDate,
+                ReferenceType = stockTransactionDto.ReferenceType,
+                ReferenceId = stockTransactionDto.ReferenceId,
+                ModifiedBy = stockTransactionDto.CreatedBy
+            });
+
+            _connection.Close();
+
+            return result;
         }
 
         public async Task<int> DeleteStockTransactionAsync(long transactionId)
